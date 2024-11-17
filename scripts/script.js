5129d5bc003e3998ed4d06783ea23a0d26768aea
Branislav Djordjevic <djbane91@gmail.com>
2024-11-17 22:06:05 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // window.alert('bla');
   observeDocument();
+  gyroscopeMove();
 });
 
 function observeDocument() {
@@ -37,4 +39,50 @@ function handleMouseMove(e) {
 
   document.documentElement.style.setProperty('--rotateX', rotateX);
   document.documentElement.style.setProperty('--rotateY', rotateY);
+}
+
+function gyroscopeMove() {
+  const updateRotation = (x, y) => {
+    console.log('tuu');
+
+    const rotateX = (y - 0.5) * 25 + 'deg';
+    const rotateY = (x - 0.5) * 25 + 'deg';
+
+    document.documentElement.style.setProperty('--rotateX', rotateX);
+    document.documentElement.style.setProperty('--rotateY', rotateY);
+  };
+
+  // Gyroscope handling (for mobile)
+  // if (window.DeviceOrientationEvent) {
+  //   console.log('fdasldfasj');
+
+  if (
+    typeof DeviceOrientationEvent !== 'undefined' &&
+    typeof DeviceOrientationEvent.requestPermission === 'function'
+  ) {
+    DeviceOrientationEvent.requestPermission()
+      .then((state) => {
+        if (state === 'granted') {
+          window.addEventListener('deviceorientation', updateRotation);
+        } else {
+          console.error('Gyroscope permission denied.');
+        }
+      })
+      .catch((error) => console.error('Gyroscope error:', error));
+  } else {
+    window.addEventListener('deviceorientation', (event) => {
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+
+      // Normalize device orientation values
+      const alpha = event.alpha / 360; // Rotation around z-axis
+      const beta = (event.beta + 180) / 360; // Rotation around x-axis
+      const gamma = (event.gamma + 90) / 180; // Rotation around y-axis
+
+      const varX = gamma; // Horizontal tilt
+      const varY = beta; // Vertical tilt
+
+      updateRotation(varX, varY);
+    });
+  }
 }
