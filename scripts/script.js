22cf8f820707c4cbde246f077023721227e3fb9e
Branislav Djordjevic <djbane91@gmail.com>
2024-11-18 21:33:26 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+  getOrientation();
   observeDocument();
-  gyroscopeMove();
 });
 
 function observeDocument() {
@@ -40,47 +40,33 @@ function handleMouseMove(e) {
   document.documentElement.style.setProperty('--rotateY', rotateY);
 }
 
-function gyroscopeMove() {
-  const updateRotation = (x, y) => {
-    console.log('tuu');
-
-    const rotateX = (y - 0.5) * 25 + 'deg';
-    const rotateY = (x - 0.5) * 25 + 'deg';
-
-    document.documentElement.style.setProperty('--rotateX', rotateX);
-    document.documentElement.style.setProperty('--rotateY', rotateY);
-  };
-
-  // Gyroscope handling (for mobile)
-  // if (window.DeviceOrientationEvent) {
-  //   console.log('fdasldfasj');
-
-  if (
-    typeof DeviceOrientationEvent !== 'undefined' &&
-    typeof DeviceOrientationEvent.requestPermission === 'function'
-  ) {
-    DeviceOrientationEvent.requestPermission()
-      .then((state) => {
-        if (state === 'granted') {
-          window.addEventListener('deviceorientation', updateRotation);
-        } else {
-          console.error('Gyroscope permission denied.');
-        }
-      })
-      .catch((error) => console.error('Gyroscope error:', error));
+function getOrientation() {
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleOrientation, true);
   } else {
-    window.addEventListener('deviceorientation', (event) => {
-      // Normalize device orientation values
-      const alpha = event.alpha / 360; // Rotation around z-axis
-      const beta = (event.beta + 180) / 360; // Rotation around x-axis
-      const gamma = (event.gamma + 90) / 180; // Rotation around y-axis
-
-      const varX = gamma; // Horizontal tilt
-      const varY = beta; // Vertical tilt
-
-      updateRotation(varX, varY);
-    });
+    alert('DeviceOrientationEvent is not supported by your browser.');
+    console.error('DeviceOrientationEvent is not supported by your browser.');
   }
+}
+
+let currentX = 0;
+let currentY = 0;
+
+function handleOrientation(event) {
+  const { beta, gamma } = event;
+
+  const screenPosY = beta / 45;
+  const screenPosX = gamma / 45;
+
+  const targetX = screenPosY * 12;
+  const targetY = screenPosX * 12;
+
+  const damping = 0.6;
+  currentX += (targetX - currentX) * damping;
+  currentY += (targetY - currentY) * damping;
+
+  document.documentElement.style.setProperty('--rotateX', -currentX + 'deg');
+  document.documentElement.style.setProperty('--rotateY', -currentY + 'deg');
 }
 
 const getCoordinate = (pos, dim) => Math.round(((pos - dim / 2) / (dim / 2)) * 100) / 100;
