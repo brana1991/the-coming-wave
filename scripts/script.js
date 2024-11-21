046011937f46ee5eb13a3cde13312547cac35b92
Branislav Djordjevic <djbane91@gmail.com>
2024-11-21 14:44:51 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -58,14 +58,14 @@ function handleOrientation(event) {
   const screenPosY = beta / 45;
   const screenPosX = gamma / 45;
 
-  const targetX = screenPosY * 12;
-  const targetY = screenPosX * 12;
+  const targetX = screenPosY * 12 + 15;
+  const targetY = screenPosX * 12 + 15;
 
   const damping = 0.6;
   currentX += (targetX - currentX) * damping;
   currentY += (targetY - currentY) * damping;
 
-  document.documentElement.style.setProperty('--rotateX', -(currentX + 15) + 'deg');
+  document.documentElement.style.setProperty('--rotateX', -currentX + 'deg');
   document.documentElement.style.setProperty('--rotateY', -currentY + 'deg');
 }
 
