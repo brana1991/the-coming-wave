7a1332c0a9048abf14cbff6ced01b2a435801bfc
Branislav Djordjevic <djbane91@gmail.com>
2024-11-29 18:42:51 +0100

    update

--- promo/talas-koji-dolazi/src/scripts/script.js
+++ promo/talas-koji-dolazi/src/scripts/script.js
@@ -47,7 +47,7 @@ function handleMouseMove(e) {
 async function getOrientation() {
   if (window.DeviceOrientationEvent) {
     const reequestPermissionButton = document.getElementById('request-gesture-permision');
-    reequestPermissionButton.addEventListener('click', requestDeviceOrientationPermission);
+    reequestPermissionButton.addEventListener('touchstart', requestDeviceOrientationPermission);
 
     if (
       typeof DeviceOrientationEvent !== 'undefined' &&
@@ -92,12 +92,12 @@ async function requestDeviceOrientationPermission() {
 
   if (permission === 'granted') {
     const reequestPermissionButton = document.getElementById('request-gesture-permision');
-    reequestPermissionButton.removeEventListener('click', requestDeviceOrientationPermission);
+    reequestPermissionButton.removeEventListener('touchstart', requestDeviceOrientationPermission);
 
     console.log('Permission granted! You can now use DeviceOrientation events.');
     window.addEventListener('deviceorientation', handleOrientation);
   } else {
-    reequestPermissionButton.removeEventListener('click', requestDeviceOrientationPermission);
+    reequestPermissionButton.removeEventListener('touchstart', requestDeviceOrientationPermission);
     console.log('Permission denied. Device orientation events will not work.');
   }
 }
